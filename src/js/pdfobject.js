b707a031155c7b4a2f3de99b4d9fe925060ae60c
Sergey Ryabov <sryabov@ptsecurity.com>
2023-12-15 00:59:49 +0400

    some improvements 2

--- src/js/pdfobject.js
+++
@@ -1,8 +0,0 @@
-var cvPdf = $(#cvPdf)
-
-
-if(PDFObject.supportsPDFs){
-   console.log("Yay, this browser supports inline PDFs.");
-} else {
-   console.log("Boo, inline PDFs are not supported by this browser");
-}
